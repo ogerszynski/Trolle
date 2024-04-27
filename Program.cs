7d855257b79df5db3c2a9b9ddf0441fb4fdbe246
ogerszynski <151456125+ogerszynski@users.noreply.github.com>
2024-04-27 16:02:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions; //wyrazenia regularne
+
+class Program
+{
+    //logika programu
+    static void Main()
+    {
+        Console.WriteLine("Wprowadź tekst:");
+        string input = Console.ReadLine(); // pobierz tekst
+        string result = RemoveVowels(input); // wywolanie funkcji usuwania samoglosek
+        Console.WriteLine("Wynik: " + result); // pokaz wynik
+    }
+// funkcja usuwania samoglosek
+    static string RemoveVowels(string input)
+    {
+        // tworzenie wyrazenia regularnego
+        Regex regex = new Regex("[aąeęioóuyAĄEĘIOÓUY]");
+        
+        // zastapienie pustym znakiem
+        string result = regex.Replace(input, "");
+        
+        // wynik
+        return result; 
+    }
+}
